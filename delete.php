eb70bdd91bd7a32cdfc116a775f9f9e5c4b138bd
lamcule <31056824+lamcule@users.noreply.github.com>
2018-09-03 19:15:44 -0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once("connect.php");
-$id = $_GET['id'];
-$sql = "delete from tbl_questions where id = $id";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-
-header("location: index.php");
- ?>
