312690368cfd05a067cb50362ae03ec25a0d108b
lamcule <31056824+lamcule@users.noreply.github.com>
2018-09-03 19:17:10 -0700

    Delete save-question.php

--- save-question.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once('connect.php');
-$ques = $_POST["question"];
-$sql = "insert into tbl_questions (question) values ('$ques')";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-header("location:index.php");
-?>
